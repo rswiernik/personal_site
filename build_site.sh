d0a7eb22555838311991d3b389978abdbab2b9e5
Reed Swiernik <rswiernik@csh.rit.edu>
2015-07-10 04:33:52 -0400

    Created build script

---
+++ build_site.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+	echo "Please provide a destination dir"
+	exit 1
+fi
+
+BUILD_DIR="."
+DEST_DIR="$1"
+
+if [[ ! -d "./.build_temp" ]]; then
+	mkdir "${BUILD_DIR}/.build_temp"
+fi
+
+jekyll build --destination "${BUILD_DIR}/.build_temp"
+
+tree "${BUILD_DIR}/.build_temp" | less
+
+read -p "Did the file structure look correct? [Y/n]" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	read -p "Would you like to copy to ${DEST_DIR}? [Y/n]" -n 1 -r
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		echo "${BUILD_DIR}/.build_temp  ->  ${DEST_DIR}"
+		cp -r "${BUILD_DIR}/.build_temp" "${DEST_DIR}"
+	else
+		echo "Aborting build..."
+	fi
+fi
+
+echo "Removing temp work directory"
+rm -r "${BUILD_DIR}/.build_temp"
